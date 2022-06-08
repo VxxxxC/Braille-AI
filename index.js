34f23931b146fefd85bbfab79016505ec42560c7
Veper(Mac) <veper.chesley@protonmail.com>
2022-06-08 18:01:03 +0800

    fix input image cannot overwrite

--- index.js
+++ index.js
@@ -2,14 +2,33 @@
 let imageContent = document.querySelector('.image-display-block');
 let imageInput = document.querySelector('.image-input');
 let innerText = document.querySelector('.inner');
+let image;
+let newImage;
 imageContent?.addEventListener('click', () => imageInput?.click());
+//-----------on click input----------------------
 imageInput?.addEventListener('change', (event) => {
     console.log(event.target.files[0]);
-    let newImage = document.createElement('img');
-    newImage.src = URL.createObjectURL(event.target.files[0]);
-    innerText.remove();
-    imageContent.appendChild(newImage);
+    if (imageContent.getElementsByTagName("img").length < 1) {
+        image = document.createElement('img');
+        image.src = URL.createObjectURL(event.target.files[0]);
+        image.style.height = '100%';
+        image.style.width = '100%';
+        image.style.objectFit = 'contain';
+        innerText.remove();
+        imageContent.appendChild(image);
+    }
+    else {
+        let image = document.querySelector('img');
+        newImage = document.createElement('img');
+        newImage.src = URL.createObjectURL(event.target.files[0]);
+        newImage.style.height = '100%';
+        newImage.style.width = '100%';
+        newImage.style.objectFit = 'contain';
+        image.remove();
+        imageContent.appendChild(newImage);
+    }
 });
+//-----------drag and drop input-----------------------
 imageContent.addEventListener("dragenter", dragenter, false);
 imageContent.addEventListener("dragover", dragover, false);
 imageContent.addEventListener("drop", drop, false);
@@ -27,12 +46,24 @@ function drop(e) {
     var dt = e.dataTransfer;
     var files = dt.files;
     console.log(files[0]);
-    let newImage = document.createElement('img');
-    newImage.src = URL.createObjectURL(files[0]);
-    newImage.style.height = '100%';
-    newImage.style.width = '100%';
-    newImage.style.objectFit = 'contain';
-    innerText.remove();
-    imageContent.appendChild(newImage);
+    if (imageContent.getElementsByTagName("img").length < 1) {
+        image = document.createElement('img');
+        image.src = URL.createObjectURL(files[0]);
+        image.style.height = '100%';
+        image.style.width = '100%';
+        image.style.objectFit = 'contain';
+        innerText.remove();
+        imageContent.appendChild(image);
+    }
+    else {
+        let image = document.querySelector('img');
+        newImage = document.createElement('img');
+        newImage.src = URL.createObjectURL(files[0]);
+        newImage.style.height = '100%';
+        newImage.style.width = '100%';
+        newImage.style.objectFit = 'contain';
+        image.remove();
+        imageContent.appendChild(newImage);
+    }
 }
-console.log(imageContent);
+console.log(imageContent.getElementsByTagName("img").length);
